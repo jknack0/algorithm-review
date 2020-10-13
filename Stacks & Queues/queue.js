98df1fe356a7968f2e6edd5be8da7a7ce349f185
jknack0 <jknack@mail.sfsu.edu>
2020-10-13 13:49:30 -0700

    implemented queue and stack

---
+++ Stacks & Queues/queue.js
@@ -0,0 +1,68 @@
+class Queue {
+  constructor() {
+    this._head = undefined
+    this._tail = undefined
+    this._length = 0
+  }
+
+  add(item) {
+    if(this._head === undefined) {
+      this._head = new QueueNode(item)
+      this._tail = this._head
+    } else {
+      const newNode = new QueueNode(item)
+      this._tail.next = newNode
+      this._tail = newNode
+    }
+
+    this._length = this._length + 1
+  }
+
+  remove() {
+    if(this._length === 0) {
+      return
+    }
+    
+    let data
+    if(this._length === 1) {
+      data = this._head
+      this._head = undefined
+      this._tail = undefined
+    } else {
+      data = this._head
+      this._head = this._head.next
+    }
+
+    this._length = this._length - 1
+    return data
+  }
+
+  peek() {
+    return this._head
+  }
+
+  isEmpty() {
+    return this._length === 0
+  }
+
+  
+}
+
+class QueueNode {
+    constructor(value, next) {
+      this._value = value
+      this._next = next ? next : null
+    }
+
+    get value() {
+      return this._value
+    }
+
+    get next() {
+      return this._next
+    }
+
+    set next(node) {
+      this._next = node
+    }
+}
---
+++ Stacks & Queues/stack.js
@@ -0,0 +1,31 @@
+class Stack{
+  constructor() {
+    this._data = []
+    this._length = 0
+  }
+
+  pop() {
+    if(this._length === 0) {
+      return
+    }
+
+    this._length = this._length - 1
+
+    return this._data.pop()
+  }
+
+  push(element) {
+    this._data.push(element)
+    this._length = this._length + 1
+  }
+
+  peek() {
+    return this._data[this._length - 1]
+  }
+
+  isEmpty() {
+    return this._length === 0
+  }
+}
+
+module.exports = Stack
