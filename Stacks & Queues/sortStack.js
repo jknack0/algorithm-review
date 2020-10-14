3158d3c174c84cee020915c0a1df444c67be8cb9
jknack0 <jknack@mail.sfsu.edu>
2020-10-14 13:05:27 -0700

    sortStack done

---
+++ Stacks & Queues/sortStack.js
@@ -0,0 +1,37 @@
+const Stack = require('./stack')
+
+// Write a program to sort a stack such that the smallest items are on the top. 
+// You can use an additional temporary stack, but you may not copy the elements 
+// into any other data structure (such as an array). The stack supports the 
+// following operations: push, pop, peek, and isEmpty.
+
+const sortStack = (stack) => {
+  let temp
+  const tempStack = new Stack()
+
+  while(!stack.isEmpty()) {
+    temp = stack.pop()
+
+    while(!tempStack.isEmpty() && tempStack.peek() < temp) {
+      stack.push(tempStack.pop())
+    }
+
+    tempStack.push(temp)
+  }
+
+  return tempStack
+}
+
+(() => {
+  const unsortedStack = new Stack()
+  unsortedStack.push(3)
+  unsortedStack.push(2)
+  unsortedStack.push(4)
+  unsortedStack.push(1)
+  unsortedStack.push(7)
+  unsortedStack.push(5)
+  unsortedStack.push(8)
+
+  console.log(sortStack(unsortedStack))
+
+})()
